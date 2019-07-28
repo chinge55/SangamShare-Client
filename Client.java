7a5273eb014eee630c59324f985d4de866d05698
sangam <sangamkhnl@gmail.com>
2019-07-28 00:10:02 -0400

    Fille buffer handling remain!

--- Client.java
+++ Client.java
@@ -82,14 +82,14 @@ public class Client
         sendmessage.start();
         readMessage.start();
 	}
-	public static void main(String[] args)
-	{
+	// public static void main(String[] args)
+	// {
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1. CHAT\n 2. RECEIVE FILE 3. SEND FILE");
-		int data = scanner.nextInt();
-		System.out.println(data);
-		Client client = new Client();	
-	}
+	// 	Scanner scanner = new Scanner(System.in);
+	// 	System.out.println("1. CHAT\n 2. RECEIVE FILE 3. SEND FILE");
+	// 	int data = scanner.nextInt();
+	// 	System.out.println(data);
+	// 	Client client = new Client();	
+	// }
 
 }
--- FileReceiveClient.java
+++ FileReceiveClient.java
@@ -20,8 +20,66 @@ public class FileReceiveClient
 			e.printStackTrace();
 		}
 		try{
+			System.out.println("Inside File Receiver");
 			datainputstream = new DataInputStream(s.getInputStream());
 			dataoutputstream = new DataOutputStream(s.getOutputStream());
+
+			Thread sendmessage = new Thread(new Runnable()
+                        {
+                                //Override
+                                public void run()
+                                {
+                                        while(true)
+                                        {
+                                                // read the message to del$
+                                                String str_msg = scanner.nextLine();
+
+                                                try
+                                                {
+                                                        dataoutputstream.writeUTF(str_msg);
+                                                }catch(IOException e)
+                                                {
+
+                                                        //e.printStackTrace();
+                                                	System.out.println("Error in Sending message");
+                                                	Thread.currentThread().interrupt();
+                                                	return;
+                                                }
+                                        }
+                                }       
+
+                        });
+			sendmessage.start();
+
+			while(true)
+			{
+				String received = datainputstream.readUTF();
+				if(received.equals("sendingafile"))
+					break;
+				else
+					System.out.println(received);
+			}
+			String filename = "";
+			filename = datainputstream.readUTF();
+			File targetFile = new File(filename);
+			OutputStream file_outputstream = new FileOutputStream(targetFile);
+			//byte [] buffer = new byte[8 * 1024];
+			//datainputstream.read(buffer);
+			
+			
+			try
+			{
+				while(true)
+				{
+					String rec;
+					rec = datainputstream.readUTF();
+					byte b[] = rec.getBytes();
+					file_outputstream.write(b);
+				}
+			}catch(Exception exception)
+			{
+				System.out.println("ERRORRR");
+			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
